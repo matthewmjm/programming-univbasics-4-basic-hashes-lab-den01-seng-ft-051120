9c87c96d1c6e1de7115a248eb32314adf021d423
Matthew Malecki <mmalecki4@gmail.com>
2020-04-16 15:52:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,16 @@
 def new_hash
-  # return an empty hash
+  hash1 = Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash2 = {"dog" => "rover", "cat" => "whiskers"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = Hash.new
+#  pioneer = {}
+  pioneer[:name] = "Grace Hopper"
+  pioneer
 end
 
 
